a2ded73a0134e0689f72068ac3b5f5ad85e49696
Ivan Kochurkin <kvanttt@gmail.com>
2025-11-09 13:57:20 +0100

    fixup! [UI] Refactor localization

--- composeApp/src/commonMain/kotlin/org/dots/game/localization/Language.kt
+++ composeApp/src/commonMain/kotlin/org/dots/game/localization/Language.kt
@@ -1,7 +1,7 @@
 package org.dots.game.localization
 
 enum class Language(val displayName: String) {
-    English( "English"),
+    English("English"),
     Russian("Русский");
 
     fun getStrings(): Strings = when (this) {
